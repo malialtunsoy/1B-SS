8f301dd5623711b79516f5268be837beeb332123
Cem Cebeci <cemcbeeci99@gmail.com>
2020-05-06 22:21:57 +0200

    Added new classes, started implementing playCombat()

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
+
 /**
  * The Player class shared by the run management and combat management subsystems.
  * Run management may want to make this a singleton class, it's only here for testing purposes right now.
  * @version 06.05.2020 14:15
  */
 public class Player {
-    
+    //constructors
+
+    //attributes
+    private ArrayList<Card> deck;
+
+    //methods
+    public void addToDeck( Card card) {
+            deck.add(card);
+    }
+
+    //returns a deep copy of the deck (the ArrayList is a deep copy but not the card objects)
+    public ArrayList<Card> getDeck() {
+        ArrayList<Card> result = new ArrayList<Card>();
+        for(Card c : deck) {
+            result.add(c);
+        }
+        return result;
+    }
 }
