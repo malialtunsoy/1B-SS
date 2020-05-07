69f7df0993dc55c07f2769b12a98e4087e457813
Can Cebeci <can.cebeci99@gmail.com>
2020-05-07 22:15:50 +0300

    Generalized Enemy and Plyaer into a superclass called CombatEntity

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,24 +1,16 @@
 import java.util.ArrayList;
 
 //Only here for testing purposes, not implemented yet.
-public class Enemy {
+public class Enemy extends CombatEntity{
     // --- attributes --- 
-    int currentHP;
-    int maxHP;
     String name;
     ArrayList<Intent> intents;
 
     // --- constructors ---
     public Enemy(String name, int maxHP) {
+        super(maxHP);
         this.name       = name;
-        this.maxHP      = maxHP;
-        this.currentHP  = maxHP; // enemies are always created at maxHP
         intents         = new ArrayList<Intent>();
     }
-
-    public void loseHP(int amount) {
-
-    }
-
 
 }
--- src/Player.java
+++ src/Player.java
@@ -5,19 +5,16 @@ import java.util.ArrayList;
  * Run management may want to make this a singleton class, it's only here for testing purposes right now.
  * @version 06.05.2020 14:15
  */
-public class Player {
+public class Player extends CombatEntity{
     //constructors
     // Test purpose constructor. Actual constructor would take the character class as input
     public Player() {
-        currentHP = 40;
-        maxHP = 40;
+        super(40);  // for now, fixed starting maxHP (40). 
         deck = new ArrayList<Card>();
     }
 
     //attributes
     private ArrayList<Card> deck;
-    private int currentHP;
-    private int maxHP;
 
     //methods
     public void addToDeck( Card card) {
@@ -33,17 +30,4 @@ public class Player {
         return result;
     }
 
-    // returns false if player dies
-    public boolean loseHP(int amount) {
-        if (amount < 0) {
-            System.out.println("loseHP called with negative amount " + amount);
-            return false;
-        }
-        currentHP -= amount;
-        if (currentHP < 0) {
-            currentHP = 0;
-            return false;
-        }
-        return true;
-    }
 }
