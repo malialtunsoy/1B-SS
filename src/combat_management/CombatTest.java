7efe906e3940991e5af65d55612a5046980255a0
Cem Cebeci <cemcbeeci99@gmail.com>
2020-05-08 11:18:45 +0200

    Organized the source files into sub-directories.

--- src/combat_management/CombatTest.java
+++ src/combat_management/CombatTest.java
@@ -1,8 +1,7 @@
-
 public class CombatTest {
     public static void main( String[] args) {
         CombatManager manager = CombatManager.getInstance();
-        manager.addEnemy(new Enemy());
+        manager.addEnemy(new Enemy("Can", 69));
         manager.setPlayer(new Player());
         System.out.println("done");
     }
