45551327360aa15eae7a59115e24135bc476a555
Mali <altunsoymali@gmail.com>
2020-05-08 13:49:55 +0300

    minor correction

--- src/CombatTest.java
+++ src/CombatTest.java
@@ -2,7 +2,7 @@
 public class CombatTest {
     public static void main( String[] args) {
         CombatManager manager = CombatManager.getInstance();
-        manager.addEnemy(new Enemy());
+        manager.addEnemy(new Enemy("error veriyo", 0));
         manager.setPlayer(new Player());
         System.out.println("done");
     }
