e7934c3ecc90aaff855c41ed956d6b55cc5e0af4
Cem Cebeci <cemcbeeci99@gmail.com>
2020-05-09 22:06:57 +0200

    Changed testing scheme, switched to a GridPane in the Combat UI.

--- src/combat_management/CombatUIAdapter.java
+++ src/combat_management/CombatUIAdapter.java
@@ -3,6 +3,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -16,7 +17,7 @@ public class CombatUIAdapter {
 
     // --- attributes ---
     Stage primaryStage;
-    FlowPane root;
+    GridPane root;
     FlowPane enemies;
 
     // --- methods ---
@@ -35,18 +36,22 @@ public class CombatUIAdapter {
     public CombatUIAdapter(Stage primaryStage){
         // set the primaryStage attribute to be able to invoke Stage.show() again
         this.primaryStage = primaryStage;
-
         // initialize the scene
-        root = new FlowPane();
+        root = new GridPane();
+        root.setGridLinesVisible(true); // for debugging purposes
         primaryStage.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT));
 
         enemies = new FlowPane();
         enemies.setHgap(20);
         enemies.setVgap(20);
-        root.getChildren().add(enemies);
+        root.add(enemies,0,0);
 
 
         //create and add the Add Enemy button
+        /* Commented out instead of deleting to revert easily.
+           These two buttons are here to set up the testing, they should be created by the CombatTest class and
+           the CombatUIAdapter should be setting its own scene, which does not include these buttons.
+
         Button addEnemyBtn = new Button();
         addEnemyBtn.setText("Add Enemy");
         addEnemyBtn.setOnAction(new EventHandler<ActionEvent>() {
@@ -57,7 +62,7 @@ public class CombatUIAdapter {
                 enemies.getChildren().add(new Text(e.toString()));
             }
         });
-        root.getChildren().add(addEnemyBtn);
+        root.add(addEnemyBtn,0,1);
 
         //create and add the Start Combat Button
         Button startCombatBtn = new Button();
@@ -69,8 +74,8 @@ public class CombatUIAdapter {
                 System.out.println("Invoked playCombat()");
             }
         });
-        root.getChildren().add(startCombatBtn);
-
+        root.add(startCombatBtn,0,2);
+        */
         //create and add the End Turn Button
         Button endTurnBtn = new Button();
         endTurnBtn.setText("End Turn");
@@ -82,8 +87,15 @@ public class CombatUIAdapter {
                 System.out.println("The player has " + CombatManager.getInstance().getPlayer().getHP() + " HP left");
             }
         });
-        root.getChildren().add(endTurnBtn);
+        root.add(endTurnBtn,1,0);
+
+        initializeCardPiles();
+
         updateView();
         primaryStage.show();
+    }
+
+    public void initializeCardPiles() {
+
     }
 }
