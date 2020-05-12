e2b5ff2e1a936c46060415f08a2683165f644d00
Can Cebeci <can.cebeci99@gmail.com>
2020-05-12 18:56:14 +0300

    (FIX) - IMPORTANT: Turns out Strike was not commited in commit 582dee6. Thus, that commit and the one after that probably will not compile. This commit should fix the issue

---
+++ src/run_management/Strike.java
@@ -0,0 +1,21 @@
+/*
+ * A concrete subclass of Card.
+ * Used in tests within Combat Management right now.
+ * author: Can Cebeci
+ */
+
+public class Strike extends Card {
+    // --- constants ---
+    private static final int ENERGY_COST = 1;
+    private static final int DAMAGE = 6;
+    private static final String DESCRIPTION = "Deals " + DAMAGE + " damage";
+
+    // constructors
+    public Strike() {
+        super("Strike", "Attack", ENERGY_COST, 0, DESCRIPTION);
+    }
+
+    public void affect(Enemy target) {
+        target.loseHP(DAMAGE);
+    }
+}
