676f17b91ddaaf480641b7b7d8d859f96e723bd8
Mali <altunsoymali@gmail.com>
2020-05-14 17:42:51 +0300

    merchant gui update. potions and relics added

--- src/run_management/MerchantController.java
+++ src/run_management/MerchantController.java
@@ -40,6 +40,12 @@ public class MerchantController implements Initializable, ControlledScreen {
     @FXML
     private VBox cardsVBox;
 
+    @FXML
+    private VBox potionsVBox;
+
+    @FXML
+    private VBox relicsVBox;
+
 
     public void setScreenParent(ScreenController screenParent){
         myController = screenParent;
@@ -53,6 +59,8 @@ public class MerchantController implements Initializable, ControlledScreen {
         maxHPLabel.setText(""+(Game.getInstance().myPlayer.getMaxHP()));
         MoneyLabel.setText(""+(Game.getInstance().myPlayer.getGold()));
 
+
+        //CARDS SETTINGS INITALIZE
         cardsVBox.setSpacing(10);
 
         int numberOfCards = 15;
@@ -130,6 +138,164 @@ public class MerchantController implements Initializable, ControlledScreen {
             //add card to HBox
             int cardHBoxPlace = (i/3);
             cardHBoxes[cardHBoxPlace].getChildren().add(cards[i]);
+
+        }
+
+
+        //POTION SETTINGS INITIALIZE  260*415
+        potionsVBox.setSpacing(10);
+
+        int numberOfPotions = 10;
+        int numberOfHBoxes = numberOfPotions/3;
+        if(numberOfPotions % 3 != 0){numberOfHBoxes++;}
+
+        HBox[] potionsHBoxes = new HBox[numberOfHBoxes];
+
+        for(int i = 0; i < numberOfHBoxes; i++){
+            potionsHBoxes[i] = new HBox();
+            potionsHBoxes[i].setAlignment(Pos.CENTER_LEFT);
+            potionsHBoxes[i].setPrefWidth(415);
+            potionsHBoxes[i].setPrefHeight(260);
+            potionsHBoxes[i].setSpacing(10);
+            potionsVBox.getChildren().add(potionsHBoxes[i]);
+        }
+
+        VBox[] potionsVBoxes = new VBox[numberOfPotions];
+        Button[] potionButton = new Button[numberOfCards];
+        for(int i = 0; i < numberOfPotions ; i++){
+            potionsVBoxes[i] = new VBox();
+            potionsVBoxes[i].setAlignment(Pos.TOP_CENTER);
+            potionsVBoxes[i].setPrefWidth(130);
+            potionsVBoxes[i].setPrefHeight(240);
+            potionsVBoxes[i].setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+
+            //add image
+            ImageView tempImage = new ImageView();
+            tempImage.setFitHeight(100);
+            tempImage.setFitWidth(100);
+            tempImage.setPickOnBounds(true);
+            tempImage.setPreserveRatio(true);
+
+            Image tempImageIn = new Image("BlockPotion.png");
+            tempImage.setImage(tempImageIn);
+
+            potionsVBoxes[i].getChildren().add(tempImage);
+            //add text
+            TextArea potionText = new TextArea();
+            potionText.setEditable(false);
+
+            potionText.setMinHeight(50);
+            potionText.setPrefWidth(130);
+            potionText.setMaxWidth(130);
+            potionText.setText("potion description  " + i);
+            //cardText.getStyleClass().add("sample");
+            potionText.setWrapText(true);
+
+            potionsVBoxes[i].getChildren().add(potionText);
+
+            //add cost and button
+
+            HBox bottomBox = new HBox();
+
+            bottomBox.setAlignment(Pos.CENTER);
+            bottomBox.setPrefHeight(44);
+            bottomBox.setPrefWidth(130);
+            bottomBox.setSpacing(10);
+
+            Text costText = new Text();
+            costText.setText("Cost: 130");
+
+
+            potionButton[i] = new Button();
+            potionButton[i].setText("Buy");
+            potionButton[i].setOnAction(e -> System.out.println("Potion  purchased."));
+
+            bottomBox.getChildren().add(costText);
+            bottomBox.getChildren().add(potionButton[i]);
+
+            potionsVBoxes[i].getChildren().add(bottomBox);
+
+            //add card to HBox
+            int potionHBoxPlace = (i/3);
+            potionsHBoxes[potionHBoxPlace].getChildren().add(potionsVBoxes[i]);
+
+        }
+
+        //RELIC SETTINGS INITIALIZE  260*415
+        relicsVBox.setSpacing(10);
+
+        int numberOfRelics = 10;
+        int numberOfHBoxesRelic = numberOfRelics/3;
+        if(numberOfRelics % 3 != 0){numberOfHBoxesRelic++;}
+
+        HBox[] relicHBoxes = new HBox[numberOfHBoxesRelic];
+
+        for(int i = 0; i < numberOfHBoxesRelic; i++){
+            relicHBoxes[i] = new HBox();
+            relicHBoxes[i].setAlignment(Pos.CENTER_LEFT);
+            relicHBoxes[i].setPrefWidth(415);
+            relicHBoxes[i].setPrefHeight(260);
+            relicHBoxes[i].setSpacing(10);
+            relicsVBox.getChildren().add(relicHBoxes[i]);
+        }
+
+        VBox[] relicsVBoxes = new VBox[numberOfRelics];
+        Button[] relicsButtons = new Button[numberOfRelics];
+        for(int i = 0; i < numberOfPotions ; i++){
+            relicsVBoxes[i] = new VBox();
+            relicsVBoxes[i].setAlignment(Pos.TOP_CENTER);
+            relicsVBoxes[i].setPrefWidth(130);
+            relicsVBoxes[i].setPrefHeight(240);
+            relicsVBoxes[i].setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+
+            //add image
+            ImageView tempImage = new ImageView();
+            tempImage.setFitHeight(100);
+            tempImage.setFitWidth(100);
+            tempImage.setPickOnBounds(true);
+            tempImage.setPreserveRatio(true);
+
+            Image tempImageIn = new Image("BurningBloodRelic.png");
+            tempImage.setImage(tempImageIn);
+
+            relicsVBoxes[i].getChildren().add(tempImage);
+            //add text
+            TextArea relicText = new TextArea();
+            relicText.setEditable(false);
+
+            relicText.setMinHeight(50);
+            relicText.setPrefWidth(130);
+            relicText.setMaxWidth(130);
+            relicText.setText("relic description  " + i);
+            relicText.setWrapText(true);
+
+            relicsVBoxes[i].getChildren().add(relicText);
+
+            //add cost and button
+
+            HBox bottomBox = new HBox();
+
+            bottomBox.setAlignment(Pos.CENTER);
+            bottomBox.setPrefHeight(44);
+            bottomBox.setPrefWidth(130);
+            bottomBox.setSpacing(10);
+
+            Text costText = new Text();
+            costText.setText("Cost: 130");
+
+
+            relicsButtons[i] = new Button();
+            relicsButtons[i].setText("Buy");
+            relicsButtons[i].setOnAction(e -> System.out.println("Relic  purchased."));
+
+            bottomBox.getChildren().add(costText);
+            bottomBox.getChildren().add(relicsButtons[i]);
+
+            relicsVBoxes[i].getChildren().add(bottomBox);
+
+            //add card to HBox
+            int relicHBoxPlace = (i/3);
+            relicHBoxes[relicHBoxPlace].getChildren().add(relicsVBoxes[i]);
 
         }
 
