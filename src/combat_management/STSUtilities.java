940987d0b374c024aa3eb70c67f97fcf532410a8
Can Cebeci <can.cebeci99@gmail.com>
2020-05-16 02:58:46 +0300

    Jaw Worm added

--- src/combat_management/STSUtilities.java
+++
@@ -1,7 +0,0 @@
-public class STSUtilities {
-    // has the input probability of returning true.
-    public static boolean trueWithProb( double prob) {
-        double rand = Math.random();
-        return rand < prob;
-    }
-}
