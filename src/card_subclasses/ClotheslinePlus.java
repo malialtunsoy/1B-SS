344b6f44e1a1b48fd821622a52776c606d0b2104
Can Cebeci <can.cebeci99@gmail.com>
2020-05-17 07:46:25 +0300

    Added Clothesline/Clothesline+ . Implemented SE "Weak"

---
+++ src/card_subclasses/ClotheslinePlus.java
@@ -0,0 +1,21 @@
+public class ClotheslinePlus extends Card {
+    // --- constants ---
+    private static final int ENERGY_COST = 2;
+    private static final int DAMAGE = 14;
+    private static final int WEAK_APPLIED = 3;
+    private static final String DESCRIPTION = "Deal " + DAMAGE + " damage. Apply " + WEAK_APPLIED + " Weak.";
+    private static final boolean TARGET_REQUIREMENT = true;
+    private static int COST = 200;
+    // constructors
+    public ClotheslinePlus() {
+        super("Clothesline+", "Attack", ENERGY_COST, DESCRIPTION, TARGET_REQUIREMENT,COST);
+    }
+
+    public void affect(Enemy target) {
+        CombatManager.getInstance().getPlayer().dealDamage(DAMAGE, target);
+        target.addStatusEffect( new Weak(WEAK_APPLIED));
+    }
+    public Card upgradedVersion() {
+        return new ClotheslinePlus();
+    }
+}
