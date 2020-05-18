0ac141f0db23b2c5e7c20d84f7db983f629c086e
Can Cebeci <can.cebeci99@gmail.com>
2020-05-18 05:20:10 +0300

    comments

--- src/status_effect_interfaces/TriggeredAtTurnEnd.java
+++ src/status_effect_interfaces/TriggeredAtTurnEnd.java
@@ -1,3 +1,4 @@
+//  triggers at different times for the player and the enemies (End of "their turn")
 public interface TriggeredAtTurnEnd extends Triggered {
     public void triggered( Object triggerSource); // called with arg. null
 }
--- src/status_effect_interfaces/TriggeredAtTurnStart.java
+++ src/status_effect_interfaces/TriggeredAtTurnStart.java
@@ -1,3 +1,4 @@
-public interface TriggeredAtTurnEnd extends Triggered {
-    public void triggered( Object triggerSource); // called with arg. null
+//  triggers at different times for the player and the enemies (Start of "their turn")
+public interface TriggeredAtTurnStart extends Triggered {
+    public void triggered(Object triggerSource); // called with arg. null
 }
