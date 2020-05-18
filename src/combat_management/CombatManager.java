abf51acb7d1f22e8d1f8f0b1a8caa36db6c0dff3
Cem Cebeci <cemcbeeci99@gmail.com>
2020-05-18 17:17:19 +0200

    Enemies are now properly removed from combat when the player dies.

--- src/combat_management/CombatManager.java
+++ src/combat_management/CombatManager.java
@@ -66,6 +66,7 @@ public class CombatManager {
 
     //the tasks that should be done at the start of every combat before the first turn is taken.
     private void initializeCombat() {
+        sceneChanged = false;
         // add relic effects
         for (int i = 0; i < player.getRelics().size(); i++) {
             player.addStatusEffect(player.getRelics().get(i).getEffect());
@@ -497,9 +498,8 @@ public class CombatManager {
     public void gameOver(){
         myController.changeScreen(RunUIManager.gameOverScreen); //need a better solution.
         stage.setScene(menu);
-
+        enemies = new ArrayList<Enemy>();
         myController.playGameOver();
-
         stage.show();
     }
 }
